6b28873deb5a7bc4a212c8f5a2f1f057c1cd7065
yarentamir <yarden44441@gmail.com>
2021-10-29 20:27:01 +0300

    commit 3

---
+++ js/myJavascript.js
@@ -0,0 +1,9 @@
+(function () {
+  "use strict";
+
+  document.getElementById("arrow-btn").addEventListener("click", function () {
+    alert("Hello World!");
+    // window.scrollTo(0, thisDiv);
+  });
+  
+})();
