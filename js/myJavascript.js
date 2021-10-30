b565ae96afb3d2e36fb022e080482319d99c27a8
yarentamir <yarden44441@gmail.com>
2021-10-31 01:18:30 +0200

    final update

---
+++ js/myJavascript.js
@@ -0,0 +1,12 @@
+// $(document).ready(function () {
+//   // arrow dwn scroll
+//   $("#arrow-btn").click(function () {
+//     $("html, body").animate(
+//       {
+//         scrollTop: $("#container2").offset().top,
+//         behavior: "smooth",
+//       },
+//       100
+//     );
+//   });
+// });
